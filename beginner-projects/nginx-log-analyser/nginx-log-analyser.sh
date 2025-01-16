0c53b4ebcb49130168976b6c51a8c4986c09cd68
dottox <ignacio.suarez.m@ibm.com>
2025-01-16 13:16:05 -0300

    feat: nginx-log-analyser added missing requirement

--- beginner-projects/nginx-log-analyser/nginx-log-analyser.sh
+++ beginner-projects/nginx-log-analyser/nginx-log-analyser.sh
@@ -15,12 +15,17 @@ echo "######################"
 echo 
 
 
-titles=("IP addresses with most requests" "most requested paths" "response status codes")
+titles=("IP addresses with most requests" "most requested paths" "response status codes" "user agents")
 info=$(cat $logfile | awk '{print $1, $7, $9}')
 
-for i in {1..3}; do
+for i in {1..4}; do
   echo "Top 5 ${titles[i-1]}:"
-  top=$(echo "$info" | awk -v indx=$i '{print $indx}' | sort | uniq -c | sort -nr | head -5)
-  awk '{print $2" - "$1" requests"}' <<< "$top"
+  if [ $i -eq 4 ]; then # user agents works differently
+    top=$(cat "$logfile" | awk -F\" -v indx=$i '{print $6}' | sort | uniq -c | sort -nr | head -5)
+    awk '{for (i=2; i<=NF; i++) printf $i " "; print "- " $1 " requests"}' <<< "$top"
+  else
+    top=$(echo "$info" | awk -v indx=$i '{print $indx}' | sort | uniq -c | sort -nr | head -5)
+    awk '{print $2" - "$1" requests"}' <<< "$top"
+  fi
   echo
 done
